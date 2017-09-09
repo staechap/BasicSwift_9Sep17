80ba2bb765e6ff440ffd3a0891141fd31367e36c
staechap <mon.oracle@gmail.com>
2017-09-09 11:53:30 +0700

    Integer

--- Vaiable and Constrant.playground/Contents.swift
+++ Vaiable and Constrant.playground/Contents.swift
@@ -8,12 +8,25 @@ var nameString = "Supachai"
 
 // เป็นการกำหนด ตัวแปร แบบ statics final
 
-let surnameString = "Taechapisit"
+var surnameString = "Taechapisit"
 
 // ลอง ทำการ assign ค่าใหม่ ให้ กับ ตัวแปร surnameString
 // ซึ่งจะ ทำไม่ได้ ต้องเปลี่ยน ให้เป็น var
 
 surnameString = "AAA"
+
+// print
+print("Name ==> \(nameString)")
+
+// About Integer
+
+let number1Aint = 5
+
+let number2Aint: Int = 10  // การประกาศแบบกำหนด type ของ data ว่าเป็น int
+
+let answer = number1Aint+number2Aint
+
+print("Answer ==>\(answer)")
 
 
 
