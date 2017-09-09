f6da9b08b118074ec9cfc6f1ee13a6aebad1596b
staechap <mon.oracle@gmail.com>
2017-09-09 14:37:12 +0700

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Array format
+
+var numberAInts = [11, 22, 33, 44, 55]
+
+print("ขนาดของ Array \(numberAInts.count)")
+
+print("numberAInts[2] ==> \(numberAInts[2+1])")
+
+print("numberAInts[4] ==> \(numberAInts[4+1])")
+
+// Add member
+
+numberAInts.append(77)
+
+// Delete member
+
+numberAInts.remove(at: 2)
+
+numberAInts
+
+
+
+
+
+
+
