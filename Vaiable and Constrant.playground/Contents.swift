fb1a64b56bbc7f32b5879abd474ec88a5755c884
oramon <mon.oracle@gmail.com>
2017-09-09 10:14:35 +0700

    test

---
+++ Vaiable and Constrant.playground/Contents.swift
@@ -0,0 +1,22 @@
+var//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var nameString = "Supachai"
+
+// เป็นการกำหนด ตัวแปร แบบ statics final
+
+let surnameString = "Taechapisit"
+
+// ลอง ทำการ assign ค่าใหม่ ให้ กับ ตัวแปร surnameString
+// ซึ่งจะ ทำไม่ได้ ต้องเปลี่ยน ให้เป็น var
+
+surnameString = "AAA"
+
+
+
+
+
+
