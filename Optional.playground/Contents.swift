fb35705529701ea4735237070b15814d902128b0
staechap <mon.oracle@gmail.com>
2017-09-10 09:23:12 +0700

    Finished

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Problem
+
+var numberString = "10"
+
+// ต้องการเอาเลข 5 มาบวก 10
+
+// var answerAInt: Int = Int(numberString)! + 10
+
+// วิธีแก้ไข number string ไม่เป็นตัวเลข
+
+var numberAInt = Int(numberString)
+
+if var numberAInt2 = numberAInt {
+    var ansAInt = numberAInt2 + 10
+}
+
+
